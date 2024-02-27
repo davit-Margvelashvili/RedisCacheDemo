0faf5ff03e6906f8b8f1610ef97572eb1236a9d7
davit.margvelashvili@gmail.com <davit.margvelashvili@gmail.com>
2024-02-28 01:51:09 +0400

    Add project files.

---
+++ RedisCacheDemo/Program.cs
@@ -0,0 +1,34 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddStackExchangeRedisOutputCache(options => { options.Configuration = builder.Configuration["RedisCacheUrl"]; });
+
+builder.Services.AddOutputCache(options =>
+{
+    options.AddBasePolicy(builder =>
+        builder.Expire(TimeSpan.FromSeconds(10)));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseOutputCache();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
